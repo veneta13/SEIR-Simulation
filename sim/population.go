058fe2ae8d62c713b6dc89c2f14096a4cc9ea1ef
Veneta <venkireva80@gmail.com>
2023-04-10 16:28:59 +0300

    Init population

---
+++ sim/population.go
@@ -0,0 +1,16 @@
+package sim
+
+type population struct {
+	susceptible int64
+	infectious  int64
+	recovered   int64
+}
+
+func createPopulation(size int64, infected int64) *population {
+	currentPopulation := population{
+		susceptible: size - infected,
+		infectious:  infected,
+		recovered:   0,
+	}
+	return &currentPopulation
+}
