4c3f2ff04f07934e3e25bba1852cef6d0f8fd35a
Veneta <venkireva80@gmail.com>
2023-04-11 20:47:30 +0300

    Init world

---
+++ sim/world.go
@@ -0,0 +1,26 @@
+package sim
+
+import "math/rand"
+
+type World struct {
+	board [WorldX][WorldY]Cell
+}
+
+func createWorld(population *Population) *World {
+	world := World{
+		board: [WorldX][WorldY]Cell{},
+	}
+
+	var current int64 = 0
+	for current < population.infectious {
+		x := rand.Int63n(WorldX)
+		y := rand.Int63n(WorldY)
+
+		if world.board[x][y] == Free {
+			world.board[x][y] = Infected
+			current++
+		}
+	}
+
+	return &world
+}
