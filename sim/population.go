372dd840a4de39cb1f764c4b1495832f19451189
Veneta <venkireva80@gmail.com>
2023-04-11 19:56:49 +0300

    Add dead counter and implement population size method

--- sim/population.go
+++ sim/population.go
@@ -1,18 +1,27 @@
 package sim
 
-type population struct {
+type Population struct {
 	susceptible int64
 	exposed     int64
 	infectious  int64
 	recovered   int64
+	dead        int64
 }
 
-func createPopulation(size int64, infected int64) *population {
-	currentPopulation := population{
+func (population *Population) size() int64 {
+	return population.susceptible +
+		population.exposed +
+		population.infectious +
+		population.recovered
+}
+
+func createPopulation(size int64, infected int64) *Population {
+	currentPopulation := Population{
 		susceptible: size - infected,
 		exposed:     0,
 		infectious:  infected,
 		recovered:   0,
+		dead:        0,
 	}
 	return &currentPopulation
 }
