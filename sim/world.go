70ef7acd420a2b73978a852ae3ef37877386f436
Veneta <venkireva80@gmail.com>
2023-04-14 20:55:09 +0300

    Change world to typedef

--- sim/world.go
+++ sim/world.go
@@ -2,22 +2,18 @@ package sim
 
 import "math/rand"
 
-type World struct {
-	board [gridSize][gridSize]Cell
-}
+type World [gridSize][gridSize]Cell
 
 func initRandomWorld(population *Population) *World {
-	world := World{
-		board: [gridSize][gridSize]Cell{},
-	}
+	world := World{}
 
 	var current int64 = 0
 	for current < population.infectious {
 		x := rand.Int63n(gridSize)
 		y := rand.Int63n(gridSize)
 
-		if world.board[x][y].cellType == Free {
-			world.board[x][y].cellType = Infected
+		if world[x][y].cellType == Free {
+			world[x][y].cellType = Infected
 			current++
 		}
 	}
